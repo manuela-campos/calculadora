86fcc9b17531629590852676a103d48fcfec7172
manuela-campos <manoelacampos1@gmail.com>
2025-03-26 08:44:44 -0300

    Atualizando a quebra de linha

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let num1;
 let num2;
 
 console.log(`Divisão: ${num1} / ${num2} = ${num1/num2}`);
-console.log(`Soma: ${num1} + ${num2} = ${num1 + num2}`); console.log(`Subtração: ${num1} - ${num2} = ${num1-num2}`);
+console.log(`Soma: ${num1} + ${num2} = ${num1 + num2}`); 
+console.log(`Subtração: ${num1} - ${num2} = ${num1-num2}`);
